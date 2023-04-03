09a6759eb92e1f341762961c9dd75b98365fa7b2
Zhao Yunlong <zao44457660@126.com>
2023-04-03 13:07:56 +0800

    update

--- EA.py
+++ EA.py
@@ -1,5 +1,6 @@
 from spoc_delivery_scheduling_evaluate_code import trappist_schedule
-
+import random
+import numpy as np
 
 #TODO: Design and implement your EA
 class myEA():
@@ -12,5 +13,44 @@ class myEA():
                      Thus you must remain and complete it.
         @return your_decision_vector: the decision vector found by your EA, 1044 dimensions
         '''
+
+        ts : trappist_schedule = trappist_schedule()
+
+        n_stations = ts.n_stations
+        n_asteriods = ts.n_asteroids
+        bounds = ts.get_bounds
+        print(bounds)
+        gap = ts.station_gap
+        db = ts.db
+
+        active_windows = np.zeros((2*n_stations))
+
+        start_time = 0
+
+        for i in range(n_stations):
+            active_windows[2*i]=start_time # start time
+            active_windows[2*i+1]=start_time + 1 # end time
+            start_time = start_time + 1 + gap
         
-        return your_decision_vector
+        assignment_pair = np.zeros((n_asteriods, 3))
+
+        for i in range(n_asteriods):
+            index = i+1
+            station_id = random.randint(0, n_stations) # 0 means unassigned
+            opportunity_id = random.randint(0, 8)
+            assignment_pair[i] = [index, station_id, opportunity_id]
+
+
+        solution = np.zeros((2*n_stations+n_asteriods*3))
+
+        solution[:2*n_stations] = active_windows
+
+        solution[2*n_stations:] = assignment_pair.flatten()
+        
+        return solution
+    
+if __name__ == "__main__":
+    udp = trappist_schedule()
+    your_decision_vector = myEA.main()
+    fitness_values = udp.fitness(your_decision_vector)
+    print(fitness_values)
